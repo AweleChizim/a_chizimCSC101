4c7f117c50edc4cbd706ad0ff55210137a768dd7
AweleChizim <115725350+AweleChizim@users.noreply.github.com>
2022-11-17 01:57:10 +0100

    (My Third Ticket) Delete target

--- week-3/practice_2/target/debug/deps/practice_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
-
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_2\target\debug\deps\practice_2.d: src\main.rs
-
-src\main.rs:
