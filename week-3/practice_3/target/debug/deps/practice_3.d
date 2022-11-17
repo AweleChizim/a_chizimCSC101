f8ed524873cc92c5cb6c0b4c2a35289d305cb04a
AweleChizim <115725350+AweleChizim@users.noreply.github.com>
2022-11-17 01:58:40 +0100

    Delete week-3/practice_3/target directory

--- week-3/practice_3/target/debug/deps/practice_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_3\target\debug\deps\practice_3.exe: src\main.rs
-
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_3\target\debug\deps\practice_3.d: src\main.rs
-
-src\main.rs:
--- week-3/practice_3/target/debug/practice_3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_3\target\debug\practice_3.exe: C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-3\practice_3\src\main.rs
