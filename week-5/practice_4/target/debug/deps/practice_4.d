b668efcd14b8f8c7a70d11f9c9292fb70b702e04
AweleChizim <115725350+AweleChizim@users.noreply.github.com>
2022-11-23 20:46:09 +0100

    Delete week-5/practice_4/target directory

--- week-5/practice_4/target/debug/deps/practice_4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-5\practice_4\target\debug\deps\practice_4.exe: src\main.rs
-
-C:\Users\chizi\OneDrive\Documents\a_chizimCSC101\week-5\practice_4\target\debug\deps\practice_4.d: src\main.rs
-
-src\main.rs:
